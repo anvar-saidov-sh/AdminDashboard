085eda3d89788e68e5e7465644fc7fce75a18e91
anvarsaidovsh <bananoanimation12@gmail.com>
2024-03-29 11:47:29 +0500

    Working on Sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -11,13 +11,23 @@ function SideBar() {
           <img src={oval} alt="" className='absolute left-5 top-3 ' />
           <img src={ovalb} alt="" className='absolute left-10 top-2' />
         </div>
-        <hr className='w-48 h-0.5 bg-gray-100 border-0 rounded md:my-10 dark:bg-[#8A7DD0]'/>
+        <hr className='w-[95%] h-0.5 bg-gray-100 border-0 rounded md:my-10 dark:bg-[#8A7DD0]'/>
         <div>
-          <LayoutDashboard/>
-          <File/>
-          <Bookmark/>
-          <StarIcon/>
-          <HeartPulseIcon/>
+          <div className='flex gap-5 items-center hover:bg-white hover:text-[#5041BC] rounded-lg p-2 '>
+              <LayoutDashboard className='text-white '/> <p className='text-white text-lg '>Dashboard</p>
+          </div>
+          <div className='flex gap-5 items-center hover:bg-white rounded-lg p-2'>
+              <File className='text-white'/> <p className='text-white text-lg'>Claims</p>
+          </div>
+          <div className='flex gap-5 items-center hover:bg-white rounded-lg p-2'>
+              <Bookmark className='text-white'/> <p className='text-white text-lg'>Biller Queue</p>
+          </div>
+          <div className='flex gap-5 items-center hover:bg-white rounded-lg p-2'>
+              <StarIcon className='text-white'/> <p className='text-white text-lg'>Subscription</p>
+          </div>
+          <div className='flex gap-5 items-center hover:bg-white rounded-lg p-2'>
+              <HeartPulseIcon className='text-white'/> <p className='text-white text-lg'>Health</p>
+          </div>
         </div>
       </div>
     </>
