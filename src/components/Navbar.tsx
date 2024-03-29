d4b264784d70b48bef7e802936723eeee862f3cd
anvarsaidovsh <bananoanimation12@gmail.com>
2024-03-29 13:49:03 +0500

    Styling navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,20 +6,20 @@ import reactsvg from '../assets/react.svg'
 function Navbar() {
   return (
     <>
-      <div className="w-[85%] float-right h-[90px] bg-white flex flex-grow items-center justify-between px-[40px]        rounded-tl-full ">
+      <div className="w-[82%] float-right h-[90px] bg-white flex flex-grow items-center justify-between px-[40px] rounded-tl-full ">
         <h1 className="text-[30px] text-[#1F192F]">Overview</h1>
-        <div className="flex items-center justify-center w-[70%] px-3 ">
+        <div className="flex items-center justify-start w-[50%] px-3 ">
           <div className="flex items-center px-[30px]">
             <input type="text" placeholder="Search" className="border-[#5041BC] border-[2px]  rounded-l-full border-r-0 p-2 outline-none w-[300px] placeholder:text-[#5041bc]" />
             <Search className="size-[43px] p-2 text-[#5041bc] border-[#5041BC] border-[2px] rounded-r-full border-l-1" />
           </div>
           <Bell />
         </div>
-          <div className="flex items-center gap-4">
-            <img src={reactsvg} alt="" className="rounded-full " />
-            <p>Full Name</p>
-            <ChevronDown/>
-          </div>
+        <div className="flex items-center gap-5">
+          <img src={reactsvg} alt="" className="rounded-full " />
+          <p>Full Name</p>
+          <ChevronDown />
+        </div>
       </div>
     </>
   )
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,28 +5,28 @@ import ovalb from '../assets/Ovalb.png'
 function SideBar() {
   return (
     <>
-      <div className='flex bg-[#5041BC] w-[15%] h-[100vh] items-center fixed flex-col justify-between'>
+      <div className='flex bg-[#5041BC] w-[18%] h-[100vh] items-center fixed flex-col gap-[30px]'>
         <div className='flex relative z-10 w-[100%] justify-center items-center h-[60px]'>
           <h2 className='text-[30px] z-10 font-bold text-white'>VENTES</h2>
           <img src={oval} alt="" className='absolute left-5 top-3 ' />
           <img src={ovalb} alt="" className='absolute left-10 top-2' />
         </div>
         <hr className='w-[95%] h-0.5 bg-gray-100 border-0 rounded md:my-10 dark:bg-[#8A7DD0]'/>
-        <div>
-          <div className='flex gap-5 items-center hover:bg-white hover:text-[#5041BC] rounded-lg p-2 '>
-              <LayoutDashboard className='text-white '/> <p className='text-white text-lg '>Dashboard</p>
+        <div className='flex flex-col  items-start gap-3'>
+          <div className='group flex gap-5 items-center hover:bg-white  rounded-lg p-2 w-[200px] '>
+              <LayoutDashboard className='text-white group-hover:text-[#5041BC]'/> <p className='text-white text-[16px] group-hover:text-[#5041BC]'>Dashboard</p>
           </div>
-          <div className='flex gap-5 items-center hover:bg-white rounded-lg p-2'>
-              <File className='text-white'/> <p className='text-white text-lg'>Claims</p>
+          <div className='group flex gap-5 items-center hover:bg-white rounded-lg p-2 w-[200px]'>
+              <File className='text-white group-hover:text-[#5041BC]'/> <p className='text-white text-[16px]  group-hover:text-[#5041BC]'>Claims</p>
           </div>
-          <div className='flex gap-5 items-center hover:bg-white rounded-lg p-2'>
-              <Bookmark className='text-white'/> <p className='text-white text-lg'>Biller Queue</p>
+          <div className='group flex gap-5 items-center hover:bg-white rounded-lg p-2 w-[200px]'>
+              <Bookmark className='text-white group-hover:text-[#5041BC]'/> <p className='text-white text-[16px] group-hover:text-[#5041BC]'>Biller Queue</p>
           </div>
-          <div className='flex gap-5 items-center hover:bg-white rounded-lg p-2'>
-              <StarIcon className='text-white'/> <p className='text-white text-lg'>Subscription</p>
+          <div className='group flex gap-5 items-center hover:bg-white rounded-lg p-2 w-[200px]'>
+              <StarIcon className='text-white group-hover:text-[#5041BC]'/> <p className='text-white text-[16px]  group-hover:text-[#5041BC]'>Subscription</p>
           </div>
-          <div className='flex gap-5 items-center hover:bg-white rounded-lg p-2'>
-              <HeartPulseIcon className='text-white'/> <p className='text-white text-lg'>Health</p>
+          <div className='group flex gap-5 items-center hover:bg-white rounded-lg p-2 w-[200px]'>
+              <HeartPulseIcon className='text-white group-hover:text-[#5041BC]'/> <p className='text-white text-[16px] group-hover:text-[#5041BC]'>Health</p>
           </div>
         </div>
       </div>
