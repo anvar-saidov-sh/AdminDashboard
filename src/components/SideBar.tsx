25437891804547598458f5cfc97de65a934d4e6c
anvarsaidovsh <bananoanimation12@gmail.com>
2024-03-29 13:50:06 +0500

    Styling navbar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -13,19 +13,19 @@ function SideBar() {
         </div>
         <hr className='w-[95%] h-0.5 bg-gray-100 border-0 rounded md:my-10 dark:bg-[#8A7DD0]'/>
         <div className='flex flex-col  items-start gap-3'>
-          <div className='group flex gap-5 items-center hover:bg-white  rounded-lg p-2 w-[200px] '>
+          <div className='group flex gap-5 items-center hover:bg-white  rounded-lg p-2 w-[200px] cursor-pointer'>
               <LayoutDashboard className='text-white group-hover:text-[#5041BC]'/> <p className='text-white text-[16px] group-hover:text-[#5041BC]'>Dashboard</p>
           </div>
-          <div className='group flex gap-5 items-center hover:bg-white rounded-lg p-2 w-[200px]'>
+          <div className='group flex gap-5 items-center hover:bg-white rounded-lg p-2 w-[200px] cursor-pointer'>
               <File className='text-white group-hover:text-[#5041BC]'/> <p className='text-white text-[16px]  group-hover:text-[#5041BC]'>Claims</p>
           </div>
-          <div className='group flex gap-5 items-center hover:bg-white rounded-lg p-2 w-[200px]'>
+          <div className='group flex gap-5 items-center hover:bg-white rounded-lg p-2 w-[200px] cursor-pointer'>
               <Bookmark className='text-white group-hover:text-[#5041BC]'/> <p className='text-white text-[16px] group-hover:text-[#5041BC]'>Biller Queue</p>
           </div>
-          <div className='group flex gap-5 items-center hover:bg-white rounded-lg p-2 w-[200px]'>
+          <div className='group flex gap-5 items-center hover:bg-white rounded-lg p-2 w-[200px] cursor-pointer'>
               <StarIcon className='text-white group-hover:text-[#5041BC]'/> <p className='text-white text-[16px]  group-hover:text-[#5041BC]'>Subscription</p>
           </div>
-          <div className='group flex gap-5 items-center hover:bg-white rounded-lg p-2 w-[200px]'>
+          <div className='group flex gap-5 items-center hover:bg-white rounded-lg p-2 w-[200px] cursor-pointer'>
               <HeartPulseIcon className='text-white group-hover:text-[#5041BC]'/> <p className='text-white text-[16px] group-hover:text-[#5041BC]'>Health</p>
           </div>
         </div>
