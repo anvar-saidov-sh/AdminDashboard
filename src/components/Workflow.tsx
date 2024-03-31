6a5dcd5fbc5bc72bc4996fc062bf5011cc653267
anvarsaidovsh <bananoanimation12@gmail.com>
2024-03-31 20:35:48 +0500

    Working on content

--- src/components/Workflow.tsx
+++ src/components/Workflow.tsx
@@ -1,6 +1,6 @@
 import { EllipsisVertical } from 'lucide-react'
 import react from '../assets/react.svg'
-import {users, amount} from '../db'
+import {users, amounts } from '../db'
 function Workflow() {
     return (
         <>
@@ -8,11 +8,11 @@ function Workflow() {
                 <h2 className="text-[20px] text-[#04103B] ">Top Sales Representative</h2>
                 <div className="w-full p-2 flex flex-col items-center gap-3">
                     {
-                        users.map((user, amount) =>(
+                        users.map((user) =>(
                             <div className="flex w-full py-4 px-3 justify-between border-slate-300 border-[2px] items-center rounded-l-3xl rounded-r-3xl">
                             <img src={react} alt="" className='rounded-full w-[40px] aspect-square ' />
                             <p className=''>{user}</p>
-                            <p>${amount}</p>
+                            <p>$500</p>
                             <p>150 products</p>
                             <p>105 premium</p>
                             <p className='text-orange-300'>+Gold</p>
