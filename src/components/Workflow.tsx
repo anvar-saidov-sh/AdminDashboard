9fd6cbfa07295167f74ef5d0c922f9d94c75fe74
anvarsaidovsh <bananoanimation12@gmail.com>
2024-03-31 19:53:45 +0500

    Working on content

--- src/components/Workflow.tsx
+++ src/components/Workflow.tsx
@@ -1,4 +1,5 @@
-
+import { EllipsisVertical } from 'lucide-react'
+import react from '../assets/react.svg'
 
 
 function Workflow() {
@@ -7,8 +8,14 @@ function Workflow() {
         <div className="w-[82%] float-right bg-white flex flex-col justify-between px-[40px]  overflow-y-auto">
             <h2 className="text-[20px] text-[#04103B] ">Top Sales Representative</h2>
             <div className="w-full p-2 flex flex-col items-center">
-                <div className="flex w-full py-4 px-3">
-                    Nicola
+                <div className="flex w-full py-4 px-3 justify-between border-slate-300 border-[2px] items-center rounded-l-3xl rounded-r-3xl">
+                    <img src={react} alt="" className='rounded-full w-[40px] aspect-square '/>
+                    <p className=''>Nicola Patrick</p>
+                    <p>$500</p>
+                    <p>150 products</p>
+                    <p>105 premium</p>
+                    <p className='text-orange-300'>+Gold</p>
+                    <EllipsisVertical className='cursor-pointer text-[#5041BC] '/>
                 </div>
             </div>
         </div>
