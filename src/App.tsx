326732dbe9a720bc572a0c0de2af7604ea942a18
anvarsaidovsh <bananoanimation12@gmail.com>
2024-03-31 18:24:16 +0500

    Working on content

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import SideBar from './components/SideBar'
+import Workflow from './components/Workflow'
 function App() {
 
   return (
     <>
       <Navbar/>
       <SideBar/>
+      <Workflow/>
     </>
   )
 }
---
+++ src/components/Workflow.tsx
@@ -0,0 +1,15 @@
+
+
+
+function Workflow() {
+  return (
+    <>
+        <div className="w-[82%] float-right bg-white flex flex-col justify-between px-[40px]  overflow-y-auto">
+            <h2 className="text-[20px] text-[#04103B] ">Top Sales Representative</h2>
+            
+        </div>
+    </>
+  )
+}
+
+export default Workflow
