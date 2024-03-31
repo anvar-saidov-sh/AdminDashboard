c7089b389e40562f55411250fad6e00a221cff10
anvarsaidovsh <bananoanimation12@gmail.com>
2024-03-31 18:27:13 +0500

    Working on content

--- src/components/Workflow.tsx
+++ src/components/Workflow.tsx
@@ -6,7 +6,11 @@ function Workflow() {
     <>
         <div className="w-[82%] float-right bg-white flex flex-col justify-between px-[40px]  overflow-y-auto">
             <h2 className="text-[20px] text-[#04103B] ">Top Sales Representative</h2>
-            
+            <div className="w-full p-2 flex flex-col items-center">
+                <div className="flex w-full py-4 px-3">
+                    Nicola
+                </div>
+            </div>
         </div>
     </>
   )
