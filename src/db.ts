b3df25bba9d1e8bf479858f7ef4a95df9b48361d
anvarsaidovsh <bananoanimation12@gmail.com>
2024-04-01 21:30:38 +0500

    Working on content

---
+++ src/db.ts
@@ -0,0 +1,21 @@
+
+export const users = [
+    "Nicolas Patrick",
+    "Cordell Edwards",
+    "Derrick Spencer",
+    "Larissa Burton"
+]
+
+export const amounts = [
+    1500, 
+    1600,
+    1700,
+    1800,
+    199
+]
+
+export const userList = {
+    s : {
+        name: "Nicolas Patrick",
+    },
+}
